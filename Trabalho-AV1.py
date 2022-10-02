0e8cebd74d59d3a57ad4112927350956f945b2b1
Clara Novaes <114842640+ClaraMelloNov@users.noreply.github.com>
2022-10-01 23:43:55 -0300

    Create Trabalho-AV1.py

---
+++ Trabalho-AV1.py
@@ -0,0 +1,14 @@
+''' 1. Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu
+peso ideal, utilizando as seguintes fórmulas:
+
+altura = float(input("Informe sua altura: \n"))
+genero = input("informe seu genero: \n")
+
+if(genero == 'F'):
+    pesoideal = (72.7 * altura) - 58
+    print("seu peso ideal é: ", pesoideal)
+elif(genero == 'M'):
+    pesoideal = (62.1 * altura) - 44.7
+    print(("seu peso ideal é: ", pesoideal))
+
+''' 
